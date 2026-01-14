226ff3be0cd59aa711906a31ed43a0799c1ae2a9
Santeri Mahlamäki <20488532+Rhymex94@users.noreply.github.com>
2026-01-14 20:19:34 +0200

    fix: disable debug mode in production

--- backend/run.py
+++ backend/run.py
@@ -1,6 +1,8 @@
+import os
 from app import create_app
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0")
+    debug = os.environ.get("ENVIRONMENT") != "production"
+    app.run(debug=debug, host="0.0.0.0")
